4460a1535d48ef52769334dae4116ea08d0d43d2
Mike S <mskramst@gmail.com>
2025-07-04 18:16:04 -0700

    Updated config folder

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,6 @@
+require("config.lazy")
+require "user.colorschemes"
+require "user.mappings"
+require('lspconfig').gopls.setup({})
+--require "user.lsp-config"
+--require'lspconfig'.pyright.setup{}
---
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,17 @@
+return {
+  'tpope/vim-fugitive',
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+  {'folke/tokyonight.nvim'},
+  {'neovim/nvim-lspconfig'},
+  {'hrsh7th/cmp-nvim-lsp'},
+  {'hrsh7th/nvim-cmp'},
+  'ray-x/go.nvim',
+  "nvim-lua/popup.nvim", 			
+  {
+    "nvim-telescope/telescope.nvim",
+    requires = { {"nvim-lua/plenary.nvim"} }
+  },
+}
+
+
+
